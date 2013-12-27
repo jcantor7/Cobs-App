cca1b3f93479d00e1d7927f20eee979c821edc8d
Jacob Cantor <jake.cantor@gmail.com>
2013-12-27 12:31:01 -0500

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Pinteresting::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+ #required for heroku
+ #Note to set this to your actual host 
+ config.action_mailer.default_url_options = { :host => 'http://cobs-app.herokuapp.com/' }
 end
